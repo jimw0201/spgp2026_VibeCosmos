ba57c99ccb4b2d9f990926db7d0aa7ec3104ca4d
jimw0201 <jimw0201@gmail.com>
2026-05-01 20:46:10 +0900

    debug build 에서만 디버그 정보와 Grid 가 보이도록 설정

--- VibeCosmos/app/src/main/java/kr/ac/tukorea/jmw/vibecosmos/app/VibeCosmosActivity.kt
+++ VibeCosmos/app/src/main/java/kr/ac/tukorea/jmw/vibecosmos/app/VibeCosmosActivity.kt
@@ -3,11 +3,13 @@ package kr.ac.tukorea.jmw.vibecosmos.app
 import kr.ac.tukorea.jmw.a2dg.activity.BaseGameActivity
 import kr.ac.tukorea.jmw.a2dg.scene.Scene
 import kr.ac.tukorea.jmw.a2dg.view.GameContext
+import kr.ac.tukorea.jmw.vibecosmos.BuildConfig
 import kr.ac.tukorea.jmw.vibecosmos.game.main.MainScene
 
 class VibeCosmosActivity : BaseGameActivity() {
-    override val drawsDebugGrid: Boolean = true
-    override val drawsDebugInfo: Boolean = true
+    override val drawsDebugGrid: Boolean = BuildConfig.DEBUG
+    override val drawsDebugInfo: Boolean = BuildConfig.DEBUG
+    override val drawsFpsGraph: Boolean = BuildConfig.DEBUG
     override fun createRootScene(gctx: GameContext): Scene {
         return MainScene(gctx)
     }
