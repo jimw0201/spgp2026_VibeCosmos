1d174caf3596ddfadf6f7c4f78e63c479c31b470
jimw0201 <jimw0201@gmail.com>
2026-05-01 19:59:09 +0900

    임시 타이틀 화면

--- VibeCosmos/app/src/main/java/kr/ac/tukorea/jmw/vibecosmos/MainActivity.kt
+++ VibeCosmos/app/src/main/java/kr/ac/tukorea/jmw/vibecosmos/MainActivity.kt
@@ -1,20 +1,29 @@
 package kr.ac.tukorea.jmw.vibecosmos
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+import kr.ac.tukorea.jmw.vibecosmos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+    }
+
+    fun onBtnStartGame(view: View) {
+        startGameActivity()
+    }
+
+    private fun startGameActivity() {
+        val intent = Intent(this, VibeCosmosActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ VibeCosmos/app/src/main/java/kr/ac/tukorea/jmw/vibecosmos/VibeCosmosActivity.kt
@@ -0,0 +1,13 @@
+package kr.ac.tukorea.jmw.vibecosmos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class VibeCosmosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
