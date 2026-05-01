78853fea74b636b059a0e21dd674f18535d33c59
jimw0201 <jimw0201@gmail.com>
2026-05-01 22:09:22 +0900

    BG 추가하여 왼쪽으로 스크롤되게

--- VibeCosmos/app/src/main/java/kr/ac/tukorea/jmw/vibecosmos/game/main/MainScene.kt
+++ VibeCosmos/app/src/main/java/kr/ac/tukorea/jmw/vibecosmos/game/main/MainScene.kt
@@ -2,6 +2,14 @@ package kr.ac.tukorea.jmw.vibecosmos.game.main
 
 import kr.ac.tukorea.jmw.a2dg.scene.Scene
 import kr.ac.tukorea.jmw.a2dg.view.GameContext
+import kr.ac.tukorea.jmw.a2dg.objects.HorzScrollBackground
+import kr.ac.tukorea.jmw.a2dg.scene.World
+import kr.ac.tukorea.jmw.vibecosmos.R
+
 
 class MainScene(gctx: GameContext) : Scene(gctx) {
+    override val clipsRect = true
+    override val world = World(arrayOf(0)).apply {
+        add(HorzScrollBackground(gctx, R.mipmap.stage_bg, -150f), 0)
+    }
 }
